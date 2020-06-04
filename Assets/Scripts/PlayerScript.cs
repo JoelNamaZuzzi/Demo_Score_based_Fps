58685e0be516b64cb07776312e3a24ecdda41b66
JoelNamaZuzzi <59820667+JoelNamaZuzzi@users.noreply.github.com>
2020-06-04 15:23:04 +0200

    Modified a few things for V1

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    
+    public InputMaster controls;
+
+    void Awake ()
+    {
+        controls.Player.Shoot.performed += ctx => Shoot();
+        controls.Player.Movement.performed += _ => Move();
+    }
+
+    void Move()
+    {
+        Debug.Log("player wanna move");
+        
+
+    }
+    
+    void Shoot()
+    {
+        Debug.Log("SHOOT");
+
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+    
+    
+}
